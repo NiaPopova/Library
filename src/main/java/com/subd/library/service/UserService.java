708e84e71fa427aac99b2c7f8f3207d360dd706f
Nia <fn45622@g.fmi.uni-sofia.bg>
2024-12-17 19:21:04 +0200

    unversioned

---
+++ src/main/java/com/subd/library/service/UserService.java
@@ -0,0 +1,19 @@
+package com.subd.library.service;
+
+import com.subd.library.entity.User;
+import com.subd.library.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
